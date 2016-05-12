1a0f333c19c764bec430f992651a35422e551310
hide <nakadah@sancom.co.jp>
2016-05-12 14:58:44 +0900

    memo newline update

--- src/CommonUtill.java
+++ src/CommonUtill.java
@@ -3,12 +3,15 @@
  */
 public class CommonUtill {
     public  static final String UTF8 = "UTF-8";
+    private static final String OS   = System.getProperty("os.name").toLowerCase();
     private static final String CRLF = "\r\n";
     private static final String LF   = "\n";
     private static final String CR   = "\r";
 
     public static String getNewlineCode() {
-        //ç°âÒÇÕå≈íËíl
-        return CRLF;
+        if (OS.startsWith("linux"))        return LF;
+        else if (OS.startsWith("mac"))     return CR;
+        else if (OS.startsWith("windows")) return CRLF;
+        return LF;
     }
 }
